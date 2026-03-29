727b8c9c3bff5c44174b8662dd4e85b378db13fc
Nandish <nandish@gmail.com>
2026-03-29 21:04:10 +0530

    Practiced Mapping the CSv file to POJO

--- src/main/java/com/demo/csv/ReadCSVFile.java
+++ src/main/java/com/demo/csv/ReadCSVFile.java
@@ -3,6 +3,8 @@ package com.demo.csv;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.opencsv.CSVReader;
@@ -14,18 +16,18 @@ public class ReadCSVFile {
 		// TODO Auto-generated method stub
 		//Code to read the CSV file in java!!  [IMP interview QA]
 		
-		File csvFile=new File("D:\\Automation\\PhoenixTestAutomation_Framework\\src\\main\\resources\\testData\\LoginCreds.csv");
-		FileReader fr=new FileReader(csvFile);
-		CSVReader csvReader=new CSVReader(fr);//CSVReader constructor
+		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+//		File csvFile=new File("D:\\Automation\\PhoenixTestAutomation_Framework\\src\\main\\resources\\testData\\LoginCreds.csv");
+//		FileReader fr=new FileReader(csvFile);
+		InputStreamReader isr=new InputStreamReader(is);
+		CSVReader csvReader=new CSVReader(isr);//CSVReader constructor
 		//Requires a Reader!! 
 		
 		List<String[]>dataList=csvReader.readAll();
 		
 		for(String[] dataArray:dataList) {
-			for(String data:dataArray) {
-				System.out.print(data+" ");
-			}
-			System.out.println("");
+			System.out.println(dataArray[0]);//First Col data
+			System.out.println(dataArray[1]);//second Col data
 		}
 
 	}
